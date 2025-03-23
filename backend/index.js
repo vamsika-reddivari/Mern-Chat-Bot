97ef52fc01f38d0911660d54d10ef58476b13ac5
vamsika-reddivari <reddivarivamsika@gmail.com>
2025-03-23 16:10:59 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,3 @@
 const express = require("express");
 const app = express();
-app.listen(5000,console.log("server is running on 5000 successfully")); 
+app.listen(5000,console.log("server is running on 5000 ")); 
