538450f67ab79f36781a2f3c898f38afe5edd3c4
vamsika-reddivari <reddivarivamsika@gmail.com>
2025-03-23 16:28:05 +0530

    added comment

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const app = express();
 app.listen(5000,console.log("server running on 5000 ")); 
-nmcjjbb
+/*at Module.load (node:internal/modules/cjs/loader:1288:32)
+    at Module._load (node:internal/modules/cjs/loader:1104:12)
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:174:12)
+    at node:internal/main/run_main_module:28:49*/
